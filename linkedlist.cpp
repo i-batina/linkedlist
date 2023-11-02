0a16cae098ba7fb744316a9383b0253c79817942
Ivan Batina <ibatina123@gmail.com>
2023-11-02 16:58:50 -0400

    Added find node/data/idx functions

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -5,19 +5,13 @@ using namespace std;
 int main() {
     LinkedList l;
 
-    for (int i = 0; i < 10; i++) {
-        l.Push(i);
+    for (int i = 1111; i < 1121; i++) {
+        l.InsertTail(i);
     }
 
     l.PrintList();
 
-    l.Pop();
-    l.Pop();
-    l.Push(37);
-
-    l.PrintList();
-
-    l.InsertNode(965, 6);
+    l.ReplaceNode(353497, 5);
 
     l.PrintList();
 
--- linkedlist.h
+++ linkedlist.h
@@ -15,23 +15,32 @@ public:
     LinkedList();
     ~LinkedList();
 
-    //Methods
+    //Mutator Methods
 
+    void InsertTail(int data);              // Adds node to tail (Push)
+    void RemoveTail();                      // Removes node at tail
+    void InsertHead(int data);              // Inserts node at head
+    void RemoveHead();                      // Removes node at head (Pop)
+    
     void InsertNode(int data, int idx);     // inserts new Node to specific index of existing node
-    void ReplaceNode(int data, Node* node); // Replaces specific Node
-    void RemoveNode(Node* node);            // Removes Node with specified data
+    void ReplaceNode(int data, int idx);    // Replaces specific index
+    void RemoveNode(int idx);               // Removes Node at index
+
+    void ReverseList();
+
+    Node* FindNodeFromIdx(int idx);         // Returns node at given index
     Node* FindNode(int data);               // Returns pointer to Node with specified data
     int FindNode(Node* node);               // Returns data associated with specific node
     int FindDataFromIdx(int idx);           // Returns the data from the index of the node
 
-    int GetSize();                             // Returns size of list
+    //void PrintNode(Node* Node);
+    void PrintList();                       // Prints the entire list from head (left) to tail (right)
+
+    // Getters
+
+    int GetSize();
     Node* GetHead();
     Node* GetTail();
-
-    void PrintNode(Node* Node);
-    void PrintList();                       // Prints the entire list from head (left) to tail (right)
-    void Push(int data);                    // Adds node to tail
-    void Pop();                             // Removes node at head
 
 private:
     Node* head_;
@@ -54,7 +63,11 @@ LinkedList::~LinkedList() {
     }
 }
 
-void LinkedList::Push(int val) {
+void LinkedList::ReverseList() {
+    // TODO: add prev pointer
+}
+
+void LinkedList::InsertTail(int val) {
     if (size_ == capacity_) return;
 
     Node* temp = new Node(val);
@@ -72,17 +85,45 @@ void LinkedList::Push(int val) {
     size_++;
 }
 
-void LinkedList::Pop() {
+void LinkedList::RemoveHead() {
     if (!GetSize()) return;
 
-    Node* temp = head_;
+    Node* temp = GetHead();
     head_ = temp->next;
     delete temp;
     size_--;
 }
 
-void LinkedList::ReplaceNode(int data, Node* node) {
-    // TODO
+void LinkedList::InsertHead(int data) {
+    Node* newNode = new Node(data);
+    if (!GetSize()) {
+        head_ = newNode;
+        tail_ = newNode;
+        size_++;
+        return;
+    }
+    newNode->next = head_;
+    head_ = newNode;
+    size_++;
+}
+
+void LinkedList::RemoveTail() {
+    if (GetSize() == 1) {
+        delete head_;
+        delete tail_;
+        size_--;
+        return;
+    }
+    RemoveNode(GetSize());
+
+}
+
+void LinkedList::ReplaceNode(int val, int idx) {
+    if (idx < 0 || idx > size_)
+        return;
+    
+    Node* temp = FindNodeFromIdx(idx);
+    temp->data = val;
 }
 
 void LinkedList::InsertNode(int val, int idx) {
@@ -90,7 +131,7 @@ void LinkedList::InsertNode(int val, int idx) {
 
     Node* newNode = new Node(val);
     if (idx == GetSize()) {
-        Push(val);
+        InsertTail(val);
         size_++;
         return;
     }
@@ -113,21 +154,22 @@ void LinkedList::InsertNode(int val, int idx) {
     size_++;
 }
 
-void LinkedList::RemoveNode(Node* node) {
-    if (!GetSize()) return; // Empty list
+void LinkedList::RemoveNode(int idx) {
+    if (!GetSize() || idx < 1 || idx > GetSize()) return; // Empty list
 
-    if (node == head_) { // If node is at head position
-        Pop();
-        size_ = 0;
+    if (idx == 1) { // If node is at head position
+        RemoveHead();
         return;
     }
 
     Node* current = head_;
-    Node* prev = current;
+    Node* prev = nullptr;
+    int count = 1;
 
-    while (current != node) {
+    while (count < idx) {
         prev = current;
         current = current->next;
+        count++;
     }
 
     prev->next = current->next;
@@ -152,7 +194,8 @@ Node* LinkedList::FindNode(int val) {
     if (head_->data == val) return GetHead();
 
     Node* current = head_;
-    while (current->data != val) current = current->next;
+    while (current->data != val) 
+        current = current->next;
 
     if (current->data == val) return current;
 
@@ -165,21 +208,43 @@ int LinkedList::FindNode(Node* node) {
     else if (tail_ == node) return tail_->data;
 
     Node* current = head_;
-    while (current != node) current = current->next;
+    while (current != node) 
+        current = current->next;
     return current->data;
 }
 
 int LinkedList::FindDataFromIdx(int idx) {
-    if (!GetSize() || idx > size_ || idx < 1) return -9999;
-    else if (idx == 1) return head_->data;
+    if (!GetSize() || idx > size_ || idx < 1) 
+        return -9999;
+    else if (idx == 1) 
+        return head_->data;
     
     Node* current = head_;
     int count = 1;
-    while (count != idx) {
+
+    while (count < idx) {
         current = current->next;
         count++;
     }
     return current->data;
+}
+
+Node* LinkedList::FindNodeFromIdx(int idx) {
+    if (!GetSize() || idx > size_ || idx < 1) 
+        return NULL;
+    else if (idx == 1) 
+        return head_;
+    else if (idx == size_) 
+        return tail_;
+
+    Node* current = head_;
+    int count = 1;
+
+    while (count < idx) {
+        current = current->next;
+        count++;
+    }
+    return current;
 }
 
 void LinkedList::PrintList() {
